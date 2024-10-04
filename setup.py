75b9d105b220dabd75f7a8a70947361232d129b6
Chathuka Kisara <127914126+kisarawc@users.noreply.github.com>
2024-10-04 20:49:29 +0530

    updates+

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='academic-paper-search-engine', 
-    version='0.1',  
-    packages=find_packages(), 
+    name='academic-paper-search-engine',
+    version='0.1',
+    packages=find_packages(),
     install_requires=[
         'pandas',
         'streamlit',
@@ -11,9 +11,14 @@ setup(
         'torch',
         'nltk',
         'scikit-learn',
-        'spacy[transformers]', 
+        'spacy[transformers]',
     ],
     dependency_links=[
         'https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.8.0/en_core_web_sm-3.8.0-py3-none-any.whl'
     ],
+    entry_points={
+        'console_scripts': [
+            'install-spacy-model = spacy.cli:download --model en_core_web_sm',
+        ],
+    },
 )
